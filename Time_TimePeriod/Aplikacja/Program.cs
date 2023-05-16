0017fdcf9126d84648e2ef216b5f559fc6c2d5d1
Michał Szura <115370163+Malamity@users.noreply.github.com>
2023-05-16 16:28:54 +0200

    Time_TimePeriod

--- Time_TimePeriod/Aplikacja/Program.cs
+++ Time_TimePeriod/Aplikacja/Program.cs
@@ -1,10 +1,143 @@
-﻿namespace Aplikacja
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Time_TimePeriod;
+
+
+namespace Aplikacja
 {
-    internal class Program
+    class Program
     {
+        static Time timePoint;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WriteYourNumber();
+        }
+
+        public static void WriteYourNumber()
+        {
+            bool canMakeOp = false;
+            while(canMakeOp != true) 
+            {
+                Console.WriteLine("Podaj godzinę w formacie h:m:s");
+
+                try
+                {
+                    timePoint = new Time(Console.ReadLine());
+                    canMakeOp = true;
+                }
+                catch (FormatException ) { Console.WriteLine("Wprowadzono złe dane"); }
+                catch (OverflowException ) { Console.WriteLine("Wprowadzono zbyt dużą lub ujemną liczbę"); }
+                catch (IndexOutOfRangeException ) { Console.WriteLine("Wprowadzono niedokładne dane"); }
+                catch (Exception) { Console.WriteLine("Wprowadzono błędne dane"); }
+
+                Console.WriteLine($"Wybrałeś punkt na osi czasu= {timePoint.ToString()}");
+                AddOrSubtract(timePoint);
+            }
+        }
+
+        public static void AddOrSubtract(Time point)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Wpisz + jeżeli chcesz dodać lub - jeżeli chcesz odjąć lub * jeżeli chcesz pomnożyć");
+            string mark;
+
+            try
+            {
+                mark = Console.ReadLine();
+            }
+            catch (Exception) { throw new ArgumentException(nameof(mark), "błąd przy wprowadzaniu znaku"); }
+
+            if(mark == "+")
+            {
+                AddTimePeriod(timePoint);
+            }
+            else if(mark == "-")
+            {
+                SubtractTimePeriod(timePoint);
+            }
+            else if (mark == "*")
+            {
+                Multiply(timePoint);
+            }
+            else
+            {
+                Console.WriteLine("Wprowadzono błedne dane");
+                AddOrSubtract(timePoint);
+            }
+        }
+
+        public static void Multiply(Time newtimepoint)
+        {
+            bool canMakeOp = false;
+            int number = 1;
+            Console.WriteLine("wpisz liczbe ile razy chcesz pomnożyć swoją godzinę");
+            while (canMakeOp == false)
+            {
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                    canMakeOp = true;
+                }
+                catch (FormatException)
+                {
+
+                    Console.WriteLine("Błędne dane, należy wprwadzić cyfrę a nie inne znaki");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Wprowadzono zbyt dużą liczbę, proszę wprowadzić mniejszą liczbę");
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Wprowadzono błedne dane, spróbuj jeszcze raz wpisując cyfre");
+                    throw new ArgumentException(nameof(number), "wprowadzono błędne dane");
+                }
+
+            }
+            Time newTimePoint = newtimepoint.Multiplication(number);
+            Console.WriteLine("Twój nowy punkt na osi czasu = " + newTimePoint.ToString());
+            AddOrSubtract(newTimePoint);
+        }
+
+        public static void SubstractTimePeriod(Time newtimepoint)
+        {
+            bool canMakeOp = false;
+            TimePeriod timeperiod = new TimePeriod();
+            while (canMakeOp != true)
+            {
+
+                Console.WriteLine("Podaj ile czasu chcesz odjąć w formacie h:m:s");
+                try
+                {
+                    timeperiod = new TimePeriod(Console.ReadLine());
+                    canMakeOp = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wprowadzono złe dane, spróbuj podobnie wpisując tylko liczby");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Wprowadzono zbyt dużą liczbę, spróbuj ponownie wpisując poprawne wielkości");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Wprowadzono niedokładne dane, muszą być wszystkie liczby i każda z nich musi być rozdzielone dwukropkiem");
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Wprowadzono błedne dane, spróbuj jeszcze raz wpisując liczby oddzielone dwukropkiem");
+                    throw new ArgumentException(nameof(timeperiod), "wprowadzono błędne dane");
+                }
+            }
+            Time newTimePoint = newtimepoint.Minus(timeperiod);
+            Console.WriteLine("Twój nowy punkt na osi czasu = " + newTimePoint.ToString());
+            AddOrSubtract(newTimePoint);
         }
     }
 }
--- Time_TimePeriod/Time_TimePeriod/TimePeriod.cs
+++ Time_TimePeriod/Time_TimePeriod/TimePeriod.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Time_TimePeriod
@@ -50,5 +52,240 @@ namespace Time_TimePeriod
                 return hours;
             }
         }
+
+        public TimePeriod(byte hours, byte minutes, long seconds)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            this.sumTime = seconds + minutes * 60 + hours * 3600;
+
+            if (Hours < 0) { throw new ArgumentException(nameof(Hours), "wartość Godziny jest ujemna"); }
+            if (Minutes < 0) { throw new ArgumentException(nameof(Minutes), "wartość Minuty jest ujemna"); }
+            if (Seconds < 0) { throw new ArgumentException(nameof(Seconds), "wartość Sekundy jest ujemna"); }
+        }
+
+        public TimePeriod(byte hours, byte minutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.sumTime = minutes * 60 + hours * 3600;
+
+            if (Hours < 0) { throw new ArgumentException(nameof(Hours), "wartość Godziny jest ujemna"); }
+            if (Minutes < 0) { throw new ArgumentException(nameof(Minutes), "wartość Minuty jest ujemna"); }
+        }
+
+        public TimePeriod(long seconds)
+        {
+            this.hours = (byte)(seconds/3600);
+            this.minutes = (byte)(seconds/60);
+            this.seconds = seconds%60;
+            this.sumTime = seconds + minutes * 60 + hours * 3600;
+
+            if (Hours < 0) { throw new ArgumentException(nameof(Hours), "wartość Godziny jest ujemna"); }
+            if (Minutes < 0) { throw new ArgumentException(nameof(Minutes), "wartość Minuty jest ujemna"); }
+            if (Seconds < 0) { throw new ArgumentException(nameof(Seconds), "wartość Sekundy jest ujemna"); }
+        }
+
+        public TimePeriod(Time left, Time right)
+        {
+            this.sumTime = left.Seconds - right.Seconds + left.Minutes * 60 - right.Minutes * 60+left.Hours*3600-right.Hours*3600;
+
+            this.hours = (byte)(sumTime/3600);
+            this.minutes = (byte)((sumTime/60)%60);
+            this.seconds = sumTime % 60;
+
+            if (Hours < 0) { throw new ArgumentException(nameof(Hours), "wartość Godziny jest ujemna"); }
+            if (Minutes < 0) { throw new ArgumentException(nameof(Minutes), "wartość Minuty jest ujemna"); }
+            if (Seconds < 0) { throw new ArgumentException(nameof(Seconds), "wartość Sekundy jest ujemna"); }
+        }
+
+        public TimePeriod(string time)
+        {
+            string[] timeInString = time.Split(':');
+            long[] timeInByteInArray = Array.ConvertAll(timeInString, long.Parse);
+            this.hours=(byte)timeInByteInArray[0];
+            this.minutes = (byte)timeInByteInArray[1];
+            this.seconds = timeInByteInArray[2];
+
+            if (Hours < 0) { throw new ArgumentException(nameof(Hours), "wartość Godziny jest ujemna"); }
+            if (Minutes < 0) { throw new ArgumentException(nameof(Minutes), "wartość Minuty jest ujemna"); }
+            if (Seconds < 0) { throw new ArgumentException(nameof(Seconds), "wartość Sekundy jest ujemna"); }
+        }
+
+        public override string ToString()
+        {
+            return Hours + ":" + Minutes + ":" + Seconds;
+        }
+
+        public bool Equals(TimePeriod other)
+        {
+            if (this.Hours == other.Hours && this.Minutes == other.Minutes && this.Seconds == other.Seconds) 
+            { 
+                return true;
+            }
+            else 
+            { 
+                return false;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj==null || !(obj is TimePeriod)) return false;
+
+            return Equals((TimePeriod)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 13;
+
+            hash = (hash*7) + Hours.GetHashCode();
+            hash = (hash * 7) + Minutes.GetHashCode();
+            hash = (hash * 7) + Seconds.GetHashCode();
+            return hash;
+        }
+
+        public static bool operator ==(TimePeriod left, TimePeriod right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(TimePeriod left, TimePeriod right)
+        {
+            return !(left == right);
+        }
+
+        public int CompareTo(TimePeriod other)
+        {
+            if(this.Hours>other.Hours) return -1;
+            if (this.Hours == other.Hours && this.Minutes == other.Minutes && this.Seconds == other.Seconds) return 0;
+            return 1;
+        }
+
+        public static bool operator <(TimePeriod left, TimePeriod right)
+        {
+            if (left.Hours < right.Hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <=(TimePeriod left, TimePeriod right)
+        {
+            if (left.Hours < right.Hours || left.Equals(right))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static bool operator >(TimePeriod left, TimePeriod right)
+        {
+            if (left.Hours > right.Hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public static bool operator >=(TimePeriod left, TimePeriod right)
+        {
+            if (left.Hours > right.Hours || left.Equals(right))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public TimePeriod Plus(TimePeriod timeToAdd)
+        {
+            this.SumTime += timeToAdd.SumTime;
+            return new TimePeriod(this.SumTime);
+        }
+
+        public TimePeriod Minus(TimePeriod timeToSubtract)
+        {
+            long sumTime = Hours * 3600 + Minutes * 60 + Seconds - timeToSubtract.SumTime;
+            while((sumTime/3600)%24 < 0)
+            {
+                sumTime += 24 * 3600;
+            }
+            while((sumTime/60)%60 < 0)
+            {
+                sumTime += 86400;
+            }
+            while (sumTime % 60 < 0)
+            {
+                sumTime += 86400;
+            }
+            return new TimePeriod(sumTime);
+        }
+
+        public static TimePeriod Plus(TimePeriod timeToAdd1, TimePeriod timeToAdd2)
+        {
+            long sum=timeToAdd1.SumTime + timeToAdd2.SumTime;
+            return new TimePeriod(sum);
+        }
+
+        public static TimePeriod Minus(TimePeriod timeToSubtract1, TimePeriod timeToSubtract2)
+        {
+            long sumTime = timeToSubtract1.Hours * 3600 + timeToSubtract1.Minutes * 60 + timeToSubtract1.Seconds - timeToSubtract2.SumTime;
+
+            while ((sumTime / 3600) % 24 < 0)
+            {
+                sumTime += 24 * 3600;
+            }
+            while ((sumTime / 60) % 60 < 0)
+            {
+                sumTime += 86400;
+            }
+            while (sumTime % 60 < 0)
+            {
+                sumTime += 86400;
+            }
+            return new TimePeriod(sumTime);
+        }
+
+        public static TimePeriod operator +(TimePeriod left,  TimePeriod right)
+        {
+            return new TimePeriod(left.SumTime + right.SumTime);
+        }
+
+        public static TimePeriod operator -(TimePeriod left, TimePeriod right)
+        {
+            long sumTime = left.Hours * 3600 + left.Minutes * 60 + left.Seconds - right.SumTime;
+
+            while ((sumTime / 3600) % 24 < 0)
+            {
+                sumTime += 24 * 3600;
+            }
+            while ((sumTime / 60) % 60 < 0)
+            {
+                sumTime += 86400;
+            }
+            while (sumTime % 60 < 0)
+            {
+                sumTime += 86400;
+            }
+            return new TimePeriod(sumTime);
+        }
+
+
     }
 }
